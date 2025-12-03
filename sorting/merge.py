4c7045a0565d7a4b3339af81aac884a4e73f35dc
spiderasasen <spiderasasen@gmail.com>
2025-12-03 13:26:54 -0500

    Merge Sort finished the merge sort section

--- sorting/merge.py
+++ sorting/merge.py
@@ -1,7 +1,36 @@
 from components.creat_list import creatingTheList
 from typing import List
 
+def mergeSort(array: List[int]) -> List[int]:
+    if len(array) <= 1:
+        return array
+    else:
+        middle = len(array) // 2
 
+        #recursion inside the mergesort function
+        left = mergeSort(array[:middle])
+        right = mergeSort(array[middle:])
+
+        #calling the merge function
+        sort: List[int] = merge(left, right)
+        print(sort)
+        return sort
+
+def merge(left: List[int], right:List[int]) -> List[int]:
+    result = []
+    i = j = 0
+    while i < len(left) and j < len(right):
+        if left[0] <= right[0]:
+            result.append(left[i])
+            i = i + 1
+        else:
+            result.append(right[j])
+            j = j + 1
+        print(result)
+    result.extend(left[i:])
+    result.extend(right[j:])
+    return result
 
 if __name__ == '__main__':
-    pass
+    nums: List[int] = creatingTheList(10)
+    nums2 = mergeSort(nums.copy())
