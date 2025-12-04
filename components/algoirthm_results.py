ba5d6ff31b558ad82a6b2b81bcdbdc1bdd3bf7ce
spiderasasen <spiderasasen@gmail.com>
2025-12-04 15:33:28 -0500

    I made a button, now im going to make more shit

--- components/algoirthm_results.py
+++ components/algoirthm_results.py
@@ -10,11 +10,14 @@ def show_algorithm(root, name, numbers, sorted_numbers):
     else:
         win = tk.Toplevel(root)
         root.algo_window = win
-        win.geometry("500x400")
+        win.geometry("500x700")
 
     win.title(f"{name} Results")
 
     text_box = tk.Text(win, wrap="word", font=("Arial", 12))
-    text_box.pack(expand=True, fill="both", padx=10, pady=10)
+    text_box.pack(expand=True, fill="both", padx=20, pady=10)
     text_box.insert(tk.END, f"Unsorted list:\n{numbers}\n\n")
     text_box.insert(tk.END, f"Sorted list:\n{sorted_numbers}\n")
+
+    exit_button = tk.Button(win, text="Leave Page", command=win.destroy, width=20)
+    exit_button.pack(pady=10)
